4147f381534728b07ae85cbfdec58c67a390d321
Oscar Junior <ojmjunior@gmail.com>
2020-11-30 18:08:21 -0300

    Mister cocktail step 4

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,32 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = set_cocktail
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail), notice: 'cocktail was successfully created.'
+    else
+      redirect_to new_cocktail_path
+    end
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: %i[index new create show] do
+    resources :doses, only: %i[new create]
+  end
+  delete 'doses/:id', to: 'doses#destroy', as: :doses_delete
 end
+
+# get '/cocktails', to: 'cocktails#index'
+# get '/cocktails/new', to: 'cocktails#new'
+# get '/cocktails/:id', to: 'cocktails#show', as: :cocktails_show
+# post '/cocktails', to: 'cocktails#create'
+# get 'cocktails/:id/doses/new', to: 'doses#new', as: :doses_new
+# post 'cocktails/:id/doses', to: 'doses#create', as: :doses_create
