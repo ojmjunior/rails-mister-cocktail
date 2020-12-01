357e13bd8c1ba9c69624031fef925c47fb814c49
Oscar Junior <ojmjunior@gmail.com>
2020-12-01 18:50:49 -0300

    mister cocktail fully functional

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,7 @@ Rails.application.routes.draw do
 
   resources :cocktails, only: %i[index new create show destroy] do
     resources :doses, only: %i[new create]
+    resources :reviews, only: %i[new create]
   end
   delete 'doses/:id', to: 'doses#destroy', as: :doses_delete
 end
-
-# get '/cocktails', to: 'cocktails#index'
-# get '/cocktails/new', to: 'cocktails#new'
-# get '/cocktails/:id', to: 'cocktails#show', as: :cocktails_show
-# post '/cocktails', to: 'cocktails#create'
-# get 'cocktails/:id/doses/new', to: 'doses#new', as: :doses_new
-# post 'cocktails/:id/doses', to: 'doses#create', as: :doses_create
